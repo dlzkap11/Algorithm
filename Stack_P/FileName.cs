e4dab346c55fa24c1d228925b6f62b2fe02b2137
dlzkap11 <dlzkap11@gmail.com>
2024-07-05 17:12:43 +0900

    힙트리 -- 힙트리 기본알고리즘

--- Stack_P/FileName.cs
+++ Stack_P/FileName.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 namespace Stack_P
 {
@@ -7,279 +8,124 @@ namespace Stack_P
     //큐 : 선입선출 (FIFO)
     //트리 = 재귀함수
 
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+
+        List<T> _heap = new List<T>();
+
+        //0logN
+        public void Push(T data)
+        {
+            //힙의 맨 끝에 새로운 데이터 삽입
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+
+            //도장깨기...
+            while (now > 0)
+            {
+
+                int next = (now - 1) / 2;
+                //now가 더 작을 경우 종료
+                if (_heap[now].CompareTo(_heap[next]) < 0)
+                    break;
+
+                //값 교체 (now가 더 클 경우
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                //다음 도장으로 이동
+                now = next;
+            }
+        }
+
+        //0logN
+        public T Pop()
+        {
+            //반환할 데이터를 따로 저장
+            T ret = _heap[0];
+
+            //마지막 데이터를 루트로 이동
+            int lastindex = _heap.Count - 1;
+            _heap[0] = _heap[lastindex];
+            _heap.RemoveAt(lastindex);
+            lastindex--;
+
+            int now = 0;
+            while (true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                //왼쪽값이 현재값보다 크면, 왼쪽으로 이동
+                if (left <= lastindex && _heap[next].CompareTo(_heap[left]) < 0) 
+                    next = left;
+
+                //오른쪽값이 현재값보다 크면, 오른쪽으로 이동
+                if (right <= lastindex && _heap[next].CompareTo(_heap[right]) < 0)
+                    next = right;
+
+                //왼 오 모두 현재값보다 작으면 종료
+                if (next == now)
+                    break;
+
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                now = next;
+
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
+    }
+
+    class Knight : IComparable<Knight>
+    {
+        public int id { get; set; }
+
+        public int CompareTo(Knight? other)
+        {
+            if (id == other.id)
+                return 0;
+            return id > other.id ? 1 : -1;
+
+        }
+    }
+
 
     class Graph
     {
 
-        #region DFS, BFS
-        int[,] adj = new int[6, 6]
+        class FileName
         {
-            {-1, 15, -1, 35, -1, -1},
-            {15, -1,  5, 10, -1, -1},
-            {-1,  5, -1, -1, -1, -1},
-            {35, 10, -1, -1,  5, -1},
-            {-1, -1, -1,  5, -1,  5},
-            {-1, -1, -1, -1,  5, -1},
-        };
-
-        List<int>[] adj2 = new List<int>[]
-        {
-            new List<int>() { 1, 3 },
-            new List<int>() { 0, 2, 3 },
-            new List<int>() { 1 },
-            new List<int>() { 0, 1, 4 },
-            new List<int>() { 3, 5 },
-            new List<int>() { 4 },
-        };
 
 
-        public void Dijikstra(int start)
-        {
-            bool[] visited = new bool[6];
-            int[] distance = new int[6];
-            int[] parent = new int[6];
-
-            Array.Fill(distance, Int32.MaxValue);
-
-            distance[start] = 0;
-            parent[start] = start;
-
-
-            while (true)
+            static void Main(string[] args)
             {
-                // 제일 좋은 후보(가장 가까이 있는)
+
+                PriorityQueue<Knight> q = new PriorityQueue<Knight>();
+                q.Push(new Knight() { id = 20 });
+                q.Push(new Knight() { id = 30 });
+                q.Push(new Knight() { id = 40 });
+                q.Push(new Knight() { id = 60 });
+                q.Push(new Knight() { id = 10 });
 
 
-                // 가장 유력한 후보의 거리와 번호를 저장한다
-                int closest = Int32.MaxValue;
-                int now = -1;
-
-                for (int i = 0; i < 6; i++)
+                while (q.Count() > 0)
                 {
-                    //이미 방문한 정점은 스킵
-                    if (visited[i])
-                        continue;
-                    //아직 발견된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
-                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
-                        continue;
-
-                    //여태껏 발견한 가장 후보 정보를 갱신
-                    closest = distance[i];
-                    now = i;
+                    Console.WriteLine(q.Pop().id);
                 }
 
-                //다음 후보가 하나도 없다
-                if (now == -1)
-                    break;
-
-
-                // 찾은 후보를 방문
-                visited[now] = true;
-
-                // 방문한 정점과 인접한 정점들을 조사
-                // 상황에 따라 발견한 최단거리를 갱신
-                for (int next = 0; next < 6; next++)
-                {
-                    // 연결되지 않은 정점 스킵
-                    if (adj[now, next] == -1)
-                        continue;
-
-                    // 이미 방문한 정점은 스킵
-                    if (visited[next])
-                        continue;
-
-                    // 새로 조사된 정점의 최단거리를 갱신
-                    int nextDist = distance[now] + adj[now, next];
-
-                    // 만약에 기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면 정보를 갱신
-                    if (nextDist < distance[next])
-                    {
-                        distance[next] = nextDist;
-                        parent[next] = now;
-                    }
-                }
             }
-        }
-
-
-        bool[] visited = new bool[6]; //처음에는 false값을 가짐
-
-
-        // 1. 우선 now부터 방문
-        // 2. now와 연결된 정점들을 하나씩 확인하며 {아직 미발견(미방문} 상태라면 방문
-
-        public void DFS(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true; // 1. 우선 now부터 방문
-
-
-
-            for (int next = 0; next < 6; next++)
-            {
-                if (adj[now, next] == 0) //연결되어있지않으면 continue
-                    continue;
-
-                if (visited[next])  //이미 방문했으면 continue
-                    continue;
-
-                DFS(next);
-
-            }
-        }
-
-
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;
-
-            foreach (int next in adj2[now])
-            {
-                if (visited[next])
-                    continue;
-                DFS2(next);
-            }
-        }
-
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS(now);
-        }
-
-        public void SearchAll2()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS2(now);
-        }
-
-        public void BFS(int start)
-        {
-
-            bool[] found = new bool[6];
-            int[] parent = new int[6];      //어디서 찾아왔는가
-            int[] distance = new int[6];    //처음에서의 거리
-
-            Queue<int> queue = new Queue<int>();
-
-            queue.Enqueue(start);
-            found[start] = true;
-
-            parent[start] = start;
-            distance[start] = 0;
-
-
-            while (queue.Count > 0)
-            {
-                int now = queue.Dequeue();
-                Console.WriteLine(now);
-
-                for (int next = 0; next < 6; next++)
-                {
-                    if (adj[now, next] == 0) //인접하지 않으면 스킵
-                        continue;
-
-                    if (found[next])    //이미 발견한 곳이면 스킵
-                        continue;
-                    queue.Enqueue(next);
-                    found[next] = true;
-                    parent[next] = now;
-                    distance[next] = distance[now] + 1;
-                }
-            }
-        }
-    }
-    #endregion
-
-    class FileName
-    {
-        static Tree<string> MakeTree()
-        {
-            Tree<string> root = new Tree<string>() { Data = "R1 개발실" };
-            {
-                Tree<string> node = new Tree<string>() { Data = "디자인팀" };
-                node.Children.Add(new Tree<string>() { Data = "전투" });
-                {
-                    Tree<string> node2 = new Tree<string>() { Data = "전문 시청팀" };
-                    node2.Children.Add(new Tree<string>() { Data = "1팀" });
-                    node2.Children.Add(new Tree<string>() { Data = "2팀" });
-                    node2.Children.Add(new Tree<string>() { Data = "3팀" });
-                    node.Children.Add(node2);
-                }
-                node.Children.Add(new Tree<string>() { Data = "경제" });
-                node.Children.Add(new Tree<string>() { Data = "스토리" });
-                root.Children.Add(node);
-
-            }
-            {
-                Tree<string> node = new Tree<string>() { Data = "프로그래밍팀" };
-                node.Children.Add(new Tree<string>() { Data = "서버" });
-                node.Children.Add(new Tree<string>() { Data = "클라" });
-                node.Children.Add(new Tree<string>() { Data = "엔진" });
-                root.Children.Add(node);
-            }
-            {
-                Tree<string> node = new Tree<string>() { Data = "아트팀" };
-                node.Children.Add(new Tree<string>() { Data = "배경" });
-                node.Children.Add(new Tree<string>() { Data = "캐릭터" });
-                node.Children.Add(new Tree<string>() { Data = "스토리" });
-                root.Children.Add(node);
-            }
-            return root;
-        }
-
-        static void PrintTree(Tree<string> root)
-        {
-            Console.WriteLine(root.Data);
-
-            foreach(Tree<string> child in root.Children)
-            {
-                PrintTree(child);
-            }
-        }
-
-        static int GetHeight(Tree<string> root)
-        {
-            int height = 0;
-
-
-            foreach(Tree<string> child in root.Children)
-            {
-                int newHeight = GetHeight(child) + 1;
-                   
-                height = Math.Max(height, newHeight); //두 값 중 더 큰 값
-            }
-
-            return height;
-        }
-
-
-        static void Main(string[] args)
-        {
-            //DFS (Depth First Search 깊이 우선 탐색)  --> 가중치가 없을 때 사용 가능
-            //BFS (Breadth First Search 너비 우선 탐색) --> 최단거리에서 많이 사용
-
-            //Graph graph = new Graph();
-
-            //graph.DFS(3);
-            //graph.DFS2(3);
-            //graph.SearchAll();
-
-            //graph.BFS(0);
-            //graph.Dijikstra(0);
-
-            Tree<string> root = MakeTree();
-
-            PrintTree(root);
-
-
-            Console.WriteLine(GetHeight(root));
-            
         }
     }
 }
